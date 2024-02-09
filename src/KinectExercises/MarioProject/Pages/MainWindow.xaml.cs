cd6a58ed36242d0ce5941bbfced5a9ec8303ca07
emre.kartal <emre.kartal@etu.uca.fr>
2024-02-09 13:08:36 +0100

    Add move left, right and jump

--- src/KinectExercises/MarioProject/Pages/MainWindow.xaml.cs
+++ src/KinectExercises/MarioProject/Pages/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,12 +21,65 @@ namespace MarioProject
     /// </summary>
     public partial class MainWindow : Window
     {
-        public bool IsOpen { get; set; } = true;
+        public bool IsOpen { get; set; } = false;
+
+        private bool isJumping = false;
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
         }
+
+        private async void JumpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isJumping)
+            {
+                isJumping = true;
+                DoubleAnimation jumpAnimation = new DoubleAnimation();
+                jumpAnimation.From = Canvas.GetBottom(marioImage);
+                jumpAnimation.To = Canvas.GetBottom(marioImage) + 150;
+                jumpAnimation.Duration = TimeSpan.FromSeconds(0.5);
+
+                DoubleAnimation fallAnimation = new DoubleAnimation();
+                fallAnimation.From = Canvas.GetBottom(marioImage) + 150;
+                fallAnimation.To = Canvas.GetBottom(marioImage);
+                fallAnimation.Duration = TimeSpan.FromSeconds(0.5);
+
+                jumpAnimation.Completed += (s, _) =>
+                {
+                    marioImage.BeginAnimation(Canvas.BottomProperty, fallAnimation);
+                };
+                marioImage.BeginAnimation(Canvas.BottomProperty, jumpAnimation);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                isJumping = false;
+            }
+        }
+
+        private void MoveLeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation moveAnimation = new DoubleAnimation();
+            moveAnimation.From = Canvas.GetLeft(marioImage);
+            moveAnimation.To = Canvas.GetLeft(marioImage) - 50;
+            moveAnimation.Duration = TimeSpan.FromSeconds(0.5);
+
+            marioImage.BeginAnimation(Canvas.LeftProperty, moveAnimation);
+
+            ((ScaleTransform)marioImage.RenderTransform).ScaleX = -1;
+        }
+
+        private void MoveRightButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation moveAnimation = new DoubleAnimation();
+            moveAnimation.From = Canvas.GetLeft(marioImage);
+            moveAnimation.To = Canvas.GetLeft(marioImage) + 50;
+            moveAnimation.Duration = TimeSpan.FromSeconds(0.5);
+
+            marioImage.BeginAnimation(Canvas.LeftProperty, moveAnimation);
+
+            ((ScaleTransform)marioImage.RenderTransform).ScaleX = 1;
+        }
+
+
     }
 }
