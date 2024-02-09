3540d6dcc1eb5f28c32aa1666848b4bb3e921c4f
Emre <Emre.kartal@etu.uca.fr>
2024-02-09 22:43:29 +0100

    Add character switch and fireball

---
+++ src/KinectExercises/MarioProject/Models/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioProject.Models
+{
+    public class Character
+    {
+        public string ImageUrl { get; }
+        public double Width { get; }
+        public double Height { get; }
+
+        public Character(string imageUrl, double width, double height)
+        {
+            ImageUrl = imageUrl;
+            Width = width;
+            Height = height;
+        }
+    }
+}
--- src/KinectExercises/MarioProject/Pages/MainWindow.xaml.cs
+++ src/KinectExercises/MarioProject/Pages/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarioProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,8 @@ namespace MarioProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Character> characters = new List<Character>();
+        private int currentCharacterIndex = 0;
         public bool IsOpen { get; set; } = false;
 
         private bool isJumping = false;
@@ -28,7 +31,16 @@ namespace MarioProject
         public MainWindow()
         {
             InitializeComponent();
+            LoadCharacters();
+            UpdateCharacterButtonContent();
             DataContext = this;
+        }
+
+        private void LoadCharacters()
+        {
+            characters.Add(new Character("/Images/mario.png", 50, 100));
+            characters.Add(new Character("/Images/luigi.png", 58, 100));
+            characters.Add(new Character("/Images/waluigi.png", 50, 110));
         }
 
         private async void JumpButton_Click(object sender, RoutedEventArgs e)
@@ -37,49 +49,131 @@ namespace MarioProject
             {
                 isJumping = true;
                 DoubleAnimation jumpAnimation = new DoubleAnimation();
-                jumpAnimation.From = Canvas.GetBottom(marioImage);
-                jumpAnimation.To = Canvas.GetBottom(marioImage) + 150;
+                jumpAnimation.From = Canvas.GetBottom(mainCharacterImage);
+                jumpAnimation.To = Canvas.GetBottom(mainCharacterImage) + 150;
                 jumpAnimation.Duration = TimeSpan.FromSeconds(0.5);
 
                 DoubleAnimation fallAnimation = new DoubleAnimation();
-                fallAnimation.From = Canvas.GetBottom(marioImage) + 150;
-                fallAnimation.To = Canvas.GetBottom(marioImage);
+                fallAnimation.From = Canvas.GetBottom(mainCharacterImage) + 150;
+                fallAnimation.To = Canvas.GetBottom(mainCharacterImage);
                 fallAnimation.Duration = TimeSpan.FromSeconds(0.5);
 
                 jumpAnimation.Completed += (s, _) =>
                 {
-                    marioImage.BeginAnimation(Canvas.BottomProperty, fallAnimation);
+                    mainCharacterImage.BeginAnimation(Canvas.BottomProperty, fallAnimation);
                 };
-                marioImage.BeginAnimation(Canvas.BottomProperty, jumpAnimation);
+                mainCharacterImage.BeginAnimation(Canvas.BottomProperty, jumpAnimation);
                 await Task.Delay(TimeSpan.FromSeconds(1));
                 isJumping = false;
             }
         }
-
         private void MoveLeftButton_Click(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation moveAnimation = new DoubleAnimation();
-            moveAnimation.From = Canvas.GetLeft(marioImage);
-            moveAnimation.To = Canvas.GetLeft(marioImage) - 50;
-            moveAnimation.Duration = TimeSpan.FromSeconds(0.5);
+            double newPosition = Canvas.GetLeft(mainCharacterImage) - 50;
+            if (newPosition >= 30)
+            {
+                DoubleAnimation moveAnimation = new DoubleAnimation();
+                moveAnimation.From = Canvas.GetLeft(mainCharacterImage);
+                moveAnimation.To = newPosition;
+                moveAnimation.Duration = TimeSpan.FromSeconds(0.35);
 
-            marioImage.BeginAnimation(Canvas.LeftProperty, moveAnimation);
+                mainCharacterImage.BeginAnimation(Canvas.LeftProperty, moveAnimation);
 
-            ((ScaleTransform)marioImage.RenderTransform).ScaleX = -1;
+                ((ScaleTransform)mainCharacterImage.RenderTransform).ScaleX = -1;
+            }
         }
 
         private void MoveRightButton_Click(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation moveAnimation = new DoubleAnimation();
-            moveAnimation.From = Canvas.GetLeft(marioImage);
-            moveAnimation.To = Canvas.GetLeft(marioImage) + 50;
-            moveAnimation.Duration = TimeSpan.FromSeconds(0.5);
+            double newPosition = Canvas.GetLeft(mainCharacterImage) + 50;
+            if (newPosition + mainCharacterImage.ActualWidth + 50 <= canvas.ActualWidth)
+            {
+                DoubleAnimation moveAnimation = new DoubleAnimation();
+                moveAnimation.From = Canvas.GetLeft(mainCharacterImage);
+                moveAnimation.To = newPosition;
+                moveAnimation.Duration = TimeSpan.FromSeconds(0.35);
 
-            marioImage.BeginAnimation(Canvas.LeftProperty, moveAnimation);
+                mainCharacterImage.BeginAnimation(Canvas.LeftProperty, moveAnimation);
 
-            ((ScaleTransform)marioImage.RenderTransform).ScaleX = 1;
+                ((ScaleTransform)mainCharacterImage.RenderTransform).ScaleX = 1;
+            }
         }
 
+        public class Fireball
+        {
+            public Image Image { get; set; }
+            public double Left { get; set; }
+            public double Top { get; set; }
+        }
+
+        private List<Fireball> fireballs = new List<Fireball>();
+
+        private void ThrowsFireball(object sender, RoutedEventArgs e)
+        {
+            Image fireballImage = new Image();
+            fireballImage.Source = new BitmapImage(new Uri("/Images/fireball.png", UriKind.Relative));
+            fireballImage.Stretch = Stretch.Fill;
+            fireballImage.Width = 30;
+            fireballImage.Height = 30;
+
+            fireballImage.RenderTransformOrigin = new Point(0.5, 0.5);
+            ScaleTransform flipTransform = new ScaleTransform(-1, 1);
+            fireballImage.RenderTransform = flipTransform;
+
+            Fireball fireball = new Fireball();
+            fireball.Image = fireballImage;
+            fireball.Left = canvas.ActualWidth - Canvas.GetRight(bowserImage) - 90;
+            fireball.Top = canvas.ActualHeight - Canvas.GetBottom(bowserImage) - 100;
+            fireballs.Add(fireball);
+
+            canvas.Children.Add(fireball.Image);
+
+            Canvas.SetLeft(fireball.Image, fireball.Left);
+            Canvas.SetTop(fireball.Image, fireball.Top);
+
+            DoubleAnimation fireballAnimation = new DoubleAnimation();
+            fireballAnimation.From = fireball.Left;
+            fireballAnimation.To = -50;
+            fireballAnimation.Duration = TimeSpan.FromSeconds(2);
+
+            fireballAnimation.Completed += (sender, e) =>
+            {
+                canvas.Children.Remove(fireball.Image);
+                fireballs.Remove(fireball);
+            };
+
+            fireball.Image.BeginAnimation(Canvas.LeftProperty, fireballAnimation);
+        }
+
+        private void ChangeMainCharacterImage_Click(object sender, RoutedEventArgs e)
+        {
+            currentCharacterIndex++;
+            if (currentCharacterIndex >= characters.Count)
+            {
+                currentCharacterIndex = 0;
+            }
+            UpdateCharacterButtonContent();
+        }
+
+        private void UpdateCharacterButtonContent()
+        {
+            int nextCharacterIndex = (currentCharacterIndex + 1) % characters.Count;
+
+            Character nextCharacter = characters[nextCharacterIndex];
+            Character currentCharacter = characters[currentCharacterIndex];
+
+            Image nextCharacterButtonImage = new Image();
+            nextCharacterButtonImage.Source = new BitmapImage(new Uri(nextCharacter.ImageUrl, UriKind.Relative));
+            nextCharacterButtonImage.Width = 30;
+            nextCharacterButtonImage.Height = 30;
+
+            changeCharacterButton.Content = nextCharacterButtonImage;
+
+            mainCharacterImage.Source = new BitmapImage(new Uri(currentCharacter.ImageUrl, UriKind.Relative));
+            mainCharacterImage.Width = currentCharacter.Width;
+            mainCharacterImage.Height = currentCharacter.Height;
+            mainCharacterImage.Stretch = Stretch.Fill;
+        }
 
     }
 }
