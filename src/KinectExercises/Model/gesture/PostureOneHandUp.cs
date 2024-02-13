da2e74be28b1215b8b921ab4389738f33b1f92c2
arthur.valin <arthur.valin@etu.uca.fr>
2024-02-13 16:54:36 +0100

    Game postures

---
+++ src/KinectExercises/Model/gesture/PostureOneHandUp.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.gesture
+{
+    public class PostureOneHandUp : Posture
+    {
+        public override string GestureName => "One hand up";
+
+        protected override bool TestPosture(Body body)
+        {
+            if (body.Joints.TryGetValue(JointType.HandRight, out var handR) &&
+                body.Joints.TryGetValue(JointType.HandLeft, out var handL) &&
+                body.Joints.TryGetValue(JointType.Head, out var head))
+            {
+                return handR.Position.Y > head.Position.Y ||
+                       handL.Position.Y > head.Position.Y;
+            }
+
+            return false;
+        }
+    }
+}
